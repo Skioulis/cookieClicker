6e836b7c8737a8f6a7fce08102851083b6096c7f
Skioulis <skioulis@gmail.com>
2024-10-30 16:40:22 +0200

    finished some dictionaries

--- main.py
+++ main.py
@@ -5,27 +5,32 @@ import timeLoop
 import timeLoop as tL
 import time
 
-def initialise():
 
+
+def initialization ():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option("detach", True)
     chrome_options.add_argument("--start-maximized")
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://orteil.dashnet.org/experiments/cookie/")
+    return driver
 
-    cookie = driver.find_element(By.XPATH, value='//*[@id="cookie"]')
-    cursor = driver.find_element(By.XPATH, value='//*[@id="buyCursor"]')
-    grandma = driver.find_element(By.XPATH, value='//*[@id="buyGrandma"]')
-    factory = driver.find_element(By.XPATH, value='//*[@id="buyFactory"]')
-    mine = driver.find_element(By.XPATH, value='//*[@id="buyMine"]')
-    shipment = driver.find_element(By.XPATH, value='//*[@id="buyShipment"]')
-    alchemy_lab = driver.find_element(By.XPATH, value='//*[@id="buyAlchemy lab"]')
-    portal = driver.find_element(By.XPATH, value='//*[@id="buyPortal"]')
-    time_machine = driver.find_element(By.XPATH, value='//*[@id="buyTime machine"]')
+def cookie_shop(inner_driver : webdriver):
+
+    cookie = inner_driver.find_element(By.XPATH, value='//*[@id="cookie"]')
+    cursor = inner_driver.find_element(By.XPATH, value='//*[@id="buyCursor"]')
+    grandma = inner_driver.find_element(By.XPATH, value='//*[@id="buyGrandma"]')
+    factory = inner_driver.find_element(By.XPATH, value='//*[@id="buyFactory"]')
+    mine = inner_driver.find_element(By.XPATH, value='//*[@id="buyMine"]')
+    shipment = inner_driver.find_element(By.XPATH, value='//*[@id="buyShipment"]')
+    alchemy_lab = inner_driver.find_element(By.XPATH, value='//*[@id="buyAlchemy lab"]')
+    portal = inner_driver.find_element(By.XPATH, value='//*[@id="buyPortal"]')
+    time_machine = inner_driver.find_element(By.XPATH, value='//*[@id="buyTime machine"]')
+
+    print("shop done") # delete
 
     return {
-        "driver":driver,
         "cookie": cookie,
         "cursor":cursor,
         "grandma":grandma,
@@ -37,35 +42,44 @@ def initialise():
         "time_machine":time_machine,
     }
 
+def get_prices(inner_driver : webdriver):
+    money = inner_driver.find_element(By.ID, value="money").text
+    cursor_price = inner_driver.find_element(By.XPATH, value='//*[@id="buyCursor"]/b').text.split(' - ')[1]
+    grandma_price =  inner_driver.find_element(By.XPATH, value='//*[@id="buyGrandma"]/b').text.split(' - ')[1]
+    factory_price =  inner_driver.find_element(By.XPATH, value='//*[@id="buyFactory"]/b').text.split(' - ')[1]
+    mine_price = inner_driver.find_element(By.XPATH, value='//*[@id="buyMine"]/b').text.split(' - ')[1]
+    shipment_price = inner_driver.find_element(By.XPATH, value='//*[@id="buyShipment"]/b').text.split(' - ')[1]
+    alchemy_lab_price = inner_driver.find_element(By.XPATH, value='//*[@id="buyAlchemy lab"]/b').text.split(' - ')[1]
+    portal_price = inner_driver.find_element(By.XPATH, value='//*[@id="buyPortal"]/b').text.split(' - ')[1]
+    time_machine_price = inner_driver.find_element(By.XPATH, value='//*[@id="buyTime machine"]/b').text.split(' - ')[1]
+
+    print("prices done")
+
+    return {
+        "money" : money,
+        "cursor_price" : cursor_price,
+        "grandma_price" : grandma_price,
+        "factory_price" : factory_price,
+        "mine_price" : mine_price,
+        "shipment_price" : shipment_price,
+        "alchemy_lab_price" : alchemy_lab_price,
+        "portal_price" : portal_price,
+        "time_machine_price" : time_machine_price
+    }
+
+driver = initialization()
+
+shop = cookie_shop(driver)
+prices = get_prices(driver)
 
 
-def cookie_loop(element):
-    tL.run_for_five_sec(element['cookie'])
+# def cookie_loop(element):
+#     tL.run_for_five_sec(element['cookie'])
 
-    def get_prices():
-        money = element['driver'].find_element(By.ID, value="money").text
-        cursor_price = element['driver'].find_element(By.XPATH, value='//*[@id="buyCursor"]/b').text.split(' - ')[1]
-        grandma_price =  element['driver'].find_element(By.XPATH, value='//*[@id="buyGrandma"]/b').text.split(' - ')[1]
-        factory_price =  element['driver'].find_element(By.XPATH, value='//*[@id="buyFactory"]/b').text.split(' - ')[1]
-        mine_price = element['driver'].find_element(By.XPATH, value='//*[@id="buyMine"]/b').text.split(' - ')[1]
-        shipment_price = element['driver'].find_element(By.XPATH, value='//*[@id="buyShipment"]/b').text.split(' - ')[1]
-        alchemy_lab_price = element['driver'].find_element(By.XPATH, value='//*[@id="buyAlchemy lab"]/b').text.split(' - ')[1]
-        portal_price = element['driver'].find_element(By.XPATH, value='//*[@id="buyPortal"]/b').text.split(' - ')[1]
-        time_machine_price = element['driver'].find_element(By.XPATH, value='//*[@id="buyTime machine"]/b').text.split(' - ')[1]
 
-        return {
-            "money" : money,
-            "cursor_price" : cursor_price,
-            "grandma_price" : grandma_price,
-            "factory_price" : factory_price,
-            "mine_price" : mine_price,
-            "shipment_price" : shipment_price,
-            "alchemy_lab_price" : alchemy_lab_price,
-            "portal_price" : portal_price,
-            "time_machine_price" : time_machine_price
-        }
 
-    print(get_prices())
+
+
         # return {
         #     "money" : money.text ,
         #     "cursor_price_int":cursor_price_int}
@@ -76,7 +90,7 @@ def cookie_loop(element):
     #     element['cursor'].click()
 
 
-elements= initialise()
+shop = cookie_shop(driver)
 
 
 
@@ -84,6 +98,6 @@ elements= initialise()
 
 
 
-cookie_loop(elements)
+# cookie_loop(elements)
 
 # print(int(cursor_price.text.replace('Cursor - ', '')))
